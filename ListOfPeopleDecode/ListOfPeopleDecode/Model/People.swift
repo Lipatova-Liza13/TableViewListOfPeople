454640296ffadd940e6a95383bd378e7ad013b0f
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-08-26 19:26:41 +0300

    Create project

---
+++ ListOfPeopleDecode/ListOfPeopleDecode/Model/People.swift
@@ -0,0 +1,9 @@
+//
+//  People.swift
+//  ListOfPeopleDecode
+//
+//  Created by Teslenko Anastasiya on 26.08.2020.
+//  Copyright © 2020 Liza Lipatova. All rights reserved.
+//
+
+import Foundation
---
+++ ListOfPeopleDecode/ListOfPeopleDecode/View/PeopleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeopleTableViewCell.swift
+//  ListOfPeopleDecode
+//
+//  Created by Teslenko Anastasiya on 26.08.2020.
+//  Copyright © 2020 Liza Lipatova. All rights reserved.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
